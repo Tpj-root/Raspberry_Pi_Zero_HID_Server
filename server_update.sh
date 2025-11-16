f90637d425f83f42ceca2a45d530ce7beb07baca
Tpj-root <trichy_hackerspace@outlook.com>
2025-11-16 13:15:37 +0530

    And Here We Go

--- server_update.sh
+++ server_update.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-# Source folder = "server" inside current script directory
+# Check root
+if [ "$EUID" -ne 0 ]; then
+    echo "Restarting with root permissions..."
+    exec sudo "$0" "$@"
+fi
+
 SCRIPT_DIR="$(dirname "$0")"
 SRC_DIR="$SCRIPT_DIR/server"
-
 DEST_DIR="/var/www/html"
 
 echo "Removing old files..."
-sudo rm -rf ${DEST_DIR}/*
+rm -rf "${DEST_DIR:?}/"*
 
 echo "Copying new files..."
-sudo cp -r ${SRC_DIR}/* ${DEST_DIR}/
+cp -r "$SRC_DIR"/* "$DEST_DIR"/
 
 echo "Update complete!"
